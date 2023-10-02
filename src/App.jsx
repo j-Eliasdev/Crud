74544815097e9416d9aad63fbb811177ea029aa2
Elias Batista <j.batistadev@gmail.com>
2023-10-02 22:54:41 +0200

    add tremor

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { ListOfUsers } from "./components/tableOfUser";
 
 function App() {
   return (
-    <div>
-      <h1>Nuestro proyecto con redux</h1>
+    <div className="flex flex-col justify-center p-16">
+      <h1 className="my-9">Nuestro proyecto con redux</h1>
       <ListOfUsers />
     </div>
   );
--- src/components/tableOfUser.jsx
+++ src/components/tableOfUser.jsx
@@ -2,126 +2,105 @@ import {
   Badge,
   Button,
   Card,
-  Flex,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableHeaderCell,
   TableRow,
-  Text,
   Title,
 } from "@tremor/react";
 
-const colors = {
-  "Ready for dispatch": "gray",
-  Cancelled: "rose",
-  Shipped: "emerald",
-};
-
-const transactions = [
+const users = [
   {
-    transactionID: "#123456",
-    user: "Lena Mayer",
-    item: "Under Armour Shorts",
-    status: "Ready for dispatch",
-    amount: "$ 49.90",
-    link: "#",
+    Id: 45,
+    name: "Peter Doe",
+    email: "peter@gmail.com",
+    github: "Peter",
   },
   {
-    transactionID: "#234567",
-    user: "Max Smith",
-    item: "Book - Wealth of Nations",
-    status: "Ready for dispatch",
-    amount: "$ 19.90",
-    link: "#",
+    Id: 4,
+    name: "Erika Casio",
+    email: "erika@gmail.com",
+    github: "Erika",
   },
   {
-    transactionID: "#345678",
-    user: "Anna Stone",
-    item: "Garmin Forerunner 945",
-    status: "Cancelled",
-    amount: "$ 499.90",
-    link: "#",
-  },
-  {
-    transactionID: "#4567890",
-    user: "Truls Cumbersome",
-    item: "Running Backpack",
-    status: "Shipped",
-    amount: "$ 89.90",
-    link: "#",
-  },
-  {
-    transactionID: "#5678901",
-    user: "Peter Pikser",
-    item: "Rolex Submariner Replica",
-    status: "Cancelled",
-    amount: "$ 299.90",
-    link: "#",
-  },
-  {
-    transactionID: "#6789012",
-    user: "Phlipp Forest",
-    item: "On Clouds Shoes",
-    status: "Ready for dispatch",
-    amount: "$ 290.90",
-    link: "#",
-  },
-  {
-    transactionID: "#78901234",
-    user: "Mara Pacemaker",
-    item: "Ortovox Backpack 40l",
-    status: "Shipped",
-    amount: "$ 150.00",
-    link: "#",
-  },
-  {
-    transactionID: "#89012345",
-    user: "Sev Major",
-    item: "Oakley Jawbreaker",
-    status: "Ready for dispatch",
-    amount: "$ 190.90",
-    link: "#",
+    Id: 5,
+    name: "Pau Torres",
+    email: "pau@gmail.com",
+    github: "Pau",
   },
 ];
 
 export function ListOfUsers() {
   return (
-    <Card className="w-3/4">
-      <Flex justifyContent="start" className="space-x-2">
-        <Title>Purchases</Title>
-        <Badge color="gray">8</Badge>
-      </Flex>
-      <Text className="mt-2">Overview of this months purchases</Text>
-
-      <Table className="mt-6">
+    <Card>
+      <Title>
+        Usuarios
+        <Badge size="lg">{users.length}</Badge>
+      </Title>
+      <Table>
         <TableHead>
           <TableRow>
-            <TableHeaderCell>Transaction ID</TableHeaderCell>
-            <TableHeaderCell>User</TableHeaderCell>
-            <TableHeaderCell>Item</TableHeaderCell>
-            <TableHeaderCell>Status</TableHeaderCell>
-            <TableHeaderCell>Amount</TableHeaderCell>
-            <TableHeaderCell>Link</TableHeaderCell>
+            <TableHeaderCell>Id</TableHeaderCell>
+            <TableHeaderCell>Name</TableHeaderCell>
+            <TableHeaderCell>Email</TableHeaderCell>
+            <TableHeaderCell>GitHub</TableHeaderCell>
+            <TableHeaderCell>Acciones</TableHeaderCell>
           </TableRow>
         </TableHead>
 
         <TableBody>
-          {transactions.map((item) => (
-            <TableRow key={item.transactionID}>
-              <TableCell>{item.transactionID}</TableCell>
-              <TableCell>{item.user}</TableCell>
-              <TableCell>{item.item}</TableCell>
-              <TableCell>
-                <Badge color={colors[item.status]} size="xs">
-                  {item.status}
-                </Badge>
+          {users.map((item) => (
+            <TableRow key={item.Id}>
+              <TableCell>{item.Id}</TableCell>
+              <TableCell className="flex items-center">
+                <img
+                  className="w-8 h-8 rounded-full mr-2"
+                  src={`https://unavatar.io/github/${item.github}`}
+                  alt={item.name}
+                />
+                {item.name}
               </TableCell>
-              <TableCell>{item.amount}</TableCell>
+              <TableCell>{item.email}</TableCell>
+              <TableCell>{item.github}</TableCell>
               <TableCell>
+                <Button
+                  className="mr-2"
+                  size="xs"
+                  variant="secondary"
+                  color="gray"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                    />
+                  </svg>
+                </Button>
                 <Button size="xs" variant="secondary" color="gray">
-                  See details
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                    />
+                  </svg>
                 </Button>
               </TableCell>
             </TableRow>
